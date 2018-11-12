0adf3f673da0a9b26b88ca50c9e3586c8b3808e3
AB <my.email@ok.com>
2018-11-12 22:34:40 +0000

    Complete ArmstrongNumber

---
+++ Numbers/armstrong-numbers/ArmstrongNumber.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ArmstrongNumbers
+{
+    public static class ArmstrongNumber
+    {
+        public static bool IsArmstrongNumber(int number)
+        {
+            int sum = 0;
+            var numberString = number.ToString();
+        
+            foreach (char c in numberString)
+            {
+                sum += (int)Math.Pow(c - 48, numberString.Length);
+            }
+            
+            return sum == number;
+        }    
+    }
+}
