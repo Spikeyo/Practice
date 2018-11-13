ff6299e6e49735adac5f4c6c9d326662069aaf29
AB <my.email@ok.com>
2018-11-13 12:26:51 +0000

    Create PhoneNumberTool

---
+++ Parsing/phone-number/PhoneNumberTool.cs
@@ -0,0 +1,61 @@
+//*************************************************************
+// Solution for the PhoneNumber exercise in Exercism.io
+//
+// ~Spikeyo
+//*************************************************************
+
+using System;
+using System.Linq;
+
+public static class PhoneNumberTool
+{
+    public static string Clean(string phoneNumber)
+    {
+        return phoneNumber.RemoveNonDigits()
+                          .ValidateAndRemoveCountryCode();
+    }
+
+    private static string RemoveNonDigits(this string s)
+    {
+        return string.Join("", s.Where(char.IsDigit));
+    }
+
+    private static string ValidateAndRemoveCountryCode(this string cleanNumber)
+    {
+        if (cleanNumber.Length != 10 
+            && cleanNumber.Length != 11)
+        {
+            throw new ArgumentException("Incorrect number of digits");
+        }
+
+        if (cleanNumber.Length == 11)
+        {
+            if (IsCountryCodeValid(cleanNumber[0]))
+            {
+                throw new ArgumentException("Country code is not valid.");
+            }
+
+            cleanNumber = cleanNumber.Remove(0, 1);
+        }
+
+        ValidateNANPNumber(cleanNumber);
+
+        return cleanNumber;
+    }
+
+    private static void ValidateNANPNumber(string cleanNumber)
+    {
+        if (!IsNDigitValid(cleanNumber[0]))
+        {
+            throw new ArgumentException("Area code is not valid.");
+        }
+
+        if (!IsNDigitValid(cleanNumber[3]))
+        {
+            throw new ArgumentException("Exchange code is not valid.");
+        }
+    }
+
+    private static bool IsCountryCodeValid(char c) => c != '1';
+    private static bool IsNDigitValid(char c) => c >= 2;
+}
