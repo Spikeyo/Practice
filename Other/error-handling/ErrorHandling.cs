5e2ead8f067d10c44f3282b899ab35f3bb768c26
AB <my.email@ok.com>
2018-11-13 09:40:53 +0000

    Create Dart and DartBoardRing

---
+++ Other/error-handling/ErrorHandling.cs
@@ -0,0 +1,35 @@
+﻿//*************************************************************
+// Solution for the ErrorHandling mini-exercise in Exercism.io
+//
+// ~Spikeyo
+//*************************************************************
+
+using System;
+using System.ComponentModel;
+using System.Linq;
+
+public static class ErrorHandling
+{
+    public static void HandleErrorByThrowingException()
+    {
+        throw new Exception();
+    }
+
+    public static int? HandleErrorByReturningNullableType(string input)
+    {
+        return int.TryParse(input, out int i) ? (int?)i : null;
+    }
+
+    public static bool HandleErrorWithOutParam(string input, out int result)
+    { 
+        return int.TryParse(input, out result);
+    } 
+
+    public static void DisposableResourcesAreDisposedWhenExceptionIsThrown(IDisposable disposableObject)
+    {
+        using (disposableObject)
+        {
+            throw new Exception();
+        }        
+    } 
+}
