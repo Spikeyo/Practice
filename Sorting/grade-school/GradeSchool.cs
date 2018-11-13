f4ac41bd81f698a8d15b16f1e3d0b2e9a14ca325
AB <my.email@ok.com>
2018-11-13 15:57:56 +0000

    Change namespace to GradeSchoolProject

--- Sorting/grade-school/GradeSchool.cs
+++ Sorting/grade-school/GradeSchool.cs
@@ -7,33 +7,36 @@
 using System.Collections.Generic;
 using System.Linq;
 
-public class GradeSchool
+namespace GradeSchoolProject
 {
-    private List<Student> _students = new List<Student>();
-
-    public void Add(string student, int grade)
+    public class GradeSchool
     {
-        _students.Add(
-            new Student
-            {
-                name = student, 
-                grade = grade 
-            });
-    }
+        private List<Student> _students = new List<Student>();
 
-    public IEnumerable<string> Roster()
-    {
-        var sortedStudents = _students.OrderBy(x => x.grade)
-                                      .ThenBy(x => x.name);
+        public void Add(string student, int grade)
+        {
+            _students.Add(
+                new Student
+                {
+                    name = student, 
+                    grade = grade 
+                });
+        }
+
+        public IEnumerable<string> Roster()
+        {
+            var sortedStudents = _students.OrderBy(x => x.grade)
+                .ThenBy(x => x.name);
         
-        return sortedStudents.Select(x => x.name);
-    }
+            return sortedStudents.Select(x => x.name);
+        }
 
-    public IEnumerable<string> Grade(int grade)
-    {
-        var sortedStudents = _students.Where(x => x.grade == grade)
-                                      .OrderBy(x => x.name);
+        public IEnumerable<string> Grade(int grade)
+        {
+            var sortedStudents = _students.Where(x => x.grade == grade)
+                .OrderBy(x => x.name);
             
-        return sortedStudents.Select(x => x.name);
+            return sortedStudents.Select(x => x.name);
+        }
     }
 }
