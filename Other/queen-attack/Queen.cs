220859dfd4b1936e1ba0bbd514167949d371a05c
AB <my.email@ok.com>
2018-11-13 09:50:57 +0000

    Create Queen and QueenAttack

---
+++ Other/queen-attack/Queen.cs
@@ -0,0 +1,11 @@
+public class Queen
+{
+    public Queen(int row, int column)
+    {
+        Row = row;
+        Column = column;
+    }
+
+    public int Row { get; }
+    public int Column { get; }
+}
---
+++ Other/queen-attack/QueenAttack.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+public static class QueenAttack
+{
+    public static bool CanAttack(Queen white, Queen black)
+    {
+        return white.Row == black.Row
+               || white.Column == black.Column
+               || DoQueensShareDiagonal(white, black);
+    }
+
+    public static Queen Create(int row, int column)
+    {
+        if (row < 0 || column < 0 || row > 7 || column > 7)
+        {
+            throw new ArgumentOutOfRangeException();
+        }
+
+        return new Queen(row, column);
+    }
+
+    private static bool DoQueensShareDiagonal(Queen first, Queen second)
+    {
+        int xDistance = Math.Abs(first.Column - second.Column);
+        int yDistance = Math.Abs(first.Row - second.Row);
+
+        return xDistance == yDistance;
+    }
+}
